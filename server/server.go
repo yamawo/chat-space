4e981480f9195e735b8da1ceb7f7297a5de564be
yamawo <y7a3m1asaki@gmail.com>
2022-01-29 23:17:00 +0900

    feat: make get/post Messages function

---
+++ server/server.go
@@ -0,0 +1,47 @@
+func (s *server) GetMessages(request *empty.Empty, stream chat.Chat_GetMessagesServer) error {
+	client := NewRedisClient()
+
+	pubsub := client.Subscribe(Channel)
+	defer pubsub.Close()
+
+	_, err := pubsub.Receive()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ch := pubsub.Channel()
+
+	// Consume messages.
+	for msg := range ch {
+		var message chat.Message
+		err := json.Unmarshal([]byte(msg.Payload), &message)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(msg.Channel, msg.Payload)
+
+		if err := stream.Send(&message); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *server) PostMessage(ctx context.Context, request *chat.Message) (*chat.Result, error) {
+	client := NewRedisClient()
+
+	if request.GetCreatedAt() == nil {
+		request.CreatedAt = ptypes.TimestampNow()
+	}
+
+	message, _ := json.Marshal(request)
+
+	_ = client.Publish(Channel, message).Err()
+
+	result := &chat.Result{
+		Result: true,
+	}
+
+	return result, nil
+}
